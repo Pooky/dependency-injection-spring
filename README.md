5827fc1671751b44a3bc23b350805e04e2f7b368
Klima Martin <10054371@gemalto.com>
2017-10-23 10:41:26 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Spring Dependency Injection example without XML
+======================================
+
+Simplest example how can Spring Dependency example works. For all configuration are used annotations without any XML files.
---
+++ src/main/java/pooky/projects/Main.java
@@ -0,0 +1,20 @@
+package pooky.projects;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Starting...");
+
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+      
+		MessagePrinter printer = context.getBean(MessagePrinter.class);
+	    printer.printMessage();
+
+	    
+	}
+
+}
